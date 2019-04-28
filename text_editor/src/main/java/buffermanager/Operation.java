318013adbf5513861fcbed7d4fe92b623246d0b0
Ruturaj Nene <nene.r@husky.neu.edu>
2019-04-28 19:25:08 -0400

    Updated Operation to add a undo method

--- text_editor/src/main/java/buffermanager/Operation.java
+++ text_editor/src/main/java/buffermanager/Operation.java
@@ -9,6 +9,15 @@ public interface Operation {
      * Method to execute the operation on the buffer.
      *
      * @param buffer the buffer
+     *
+     * @return the updated buffer
      */
     StringBuilder execute(StringBuilder buffer);
+
+    /**
+     * Method to undo the current operation on the buffer
+     * @param buffer the buffer
+     * @return the updated buffer
+     */
+    StringBuilder undo(StringBuilder buffer);
 }
